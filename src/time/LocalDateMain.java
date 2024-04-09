0114c95d6cf49df78c700ba30736f2d0e4e15588
gpol2300 <gpol123@naver.com>
2024-04-09 15:06:25 +0900

    인프런 자바 중급

--- src/time/LocalDateMain.java
+++ src/time/LocalDateMain.java
@@ -6,12 +6,15 @@ public class LocalDateMain {
 
     public static void main(String[] args) {
         LocalDate nowDate = LocalDate.now();
+        //now() ,of()
         LocalDate ofDate = LocalDate.of(2013, 11, 21);
         System.out.println("오늘 날짜=" + nowDate);
         System.out.println("지정 날짜=" + ofDate);
 
         //계산(불변)
-        ofDate = ofDate.plusDays(10);
+        LocalDate ofDate1 = ofDate.plusDays(10);
+        //plusYears(), plusMonths(), plusWeeks(), plusDays()
         System.out.println("지정 날짜+10d = " + ofDate);
+        System.out.println("지정 날짜+10d = " + ofDate1);
     }
 }
