0d2545a64ea27fbe91e556f8dab5e985beacc2b0
gpol2300 <gpol123@naver.com>
2024-04-12 18:28:34 +0900

    인프런 자바 중급

--- src/exception/basic/unchecked/Client.java
+++ src/exception/basic/unchecked/Client.java
@@ -1,7 +1,7 @@
 package exception.basic.unchecked;
 
 public class Client {
-    public void call() {
+    public void call() { //런타임 예외는 throws로 예외를 던지지 않아도 된다.
         throw new MyUncheckedException("ex");
     }
 }
