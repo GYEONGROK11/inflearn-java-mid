b5d81b74ee374d5fb41993365a5dc6915c414456
gpol2300 <gpol123@naver.com>
2024-04-12 16:41:35 +0900

    인프런 자바 중급

--- src/exception/ex1/NetworkServiceV1_1.java
+++ src/exception/ex1/NetworkServiceV1_1.java
@@ -5,6 +5,8 @@ public class NetworkServiceV1_1 {
     public void sendMessage(String data) {
         String address = "http://example.com";
         NetworkClientV1 client = new NetworkClientV1(address);
+
+        // 1_1은 연결이 실패해도 데이터 전송함,오류로그도 보고싶음 - 1_2에서 수정
         client.initError(data); //추가
 
         client.connect();
--- src/exception/ex1/NetworkServiceV1_2.java
+++ src/exception/ex1/NetworkServiceV1_2.java
@@ -1,5 +1,7 @@
 package exception.ex1;
 
+import java.util.List;
+
 public class NetworkServiceV1_2 {
 
     public void sendMessage(String data) {
@@ -7,22 +9,22 @@ public class NetworkServiceV1_2 {
         NetworkClientV1 client = new NetworkClientV1(address);
         client.initError(data); //추가
 
-        String connectResult = client.connect();
+        String connectResult = client.connect(); //리턴값을 받아서 처리, 연결 성공이 아니면 에러
         if (isError(connectResult)) {
             System.out.println("[네트워크 오류 발생] 오류 코드: " + connectResult);
             return;
         }
 
-        String sendResult = client.send(data);
+        String sendResult = client.send(data);  //리턴값을 받아서 처리, 전송 성공이 아니면 에러
         if (isError(sendResult)) {
             System.out.println("[네트워크 오류 발생] 오류 코드: " + sendResult);
             return;
         }
-
+        // 1_2은 실패하면 리턴 항상 서버 연결 해제를 해야됨 - 1_3에서 수정
         client.disconnect();
     }
 
     private static boolean isError(String connectResult) {
-        return !connectResult.equals("success");
+        return !connectResult.equals("success");  //연결 성공이 아니면 에러
     }
 }
