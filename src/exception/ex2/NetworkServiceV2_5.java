b62b91617ca6ec3d31b229bdb479fff2aca19872
gpol2300 <gpol123@naver.com>
2024-04-12 19:17:38 +0900

    인프런 자바 중급

--- src/exception/ex2/NetworkServiceV2_5.java
+++ src/exception/ex2/NetworkServiceV2_5.java
@@ -13,7 +13,7 @@ public class NetworkServiceV2_5 {
             client.send(data);
         } catch (NetworkClientExceptionV2 e) {
             System.out.println("[오류] 코드: " + e.getErrorCode() + ", 메시지: " + e.getMessage());
-        } finally {  //반드시 호출해야 하는 마무리 흐름 - 주로 try에서 사용한 자원을 닫을 때 사용
+        } finally {  //반드시 호출해야 하는 마무리 흐름 - 주로 try에서 사용한 자원을 닫을 때 사용, catch에 잡혀 throw로 빠지기 전에 실행됨
             client.disconnect();
         }
 
