db3ab87aab9ca98c754544b4cd5f5a4b0420113c
gpol2300 <gpol123@naver.com>
2024-04-22 12:45:34 +0900

    인프런 자바 중급

--- src/exception/ex4/NetworkServiceV5.java
+++ src/exception/ex4/NetworkServiceV5.java
@@ -5,7 +5,8 @@ public class NetworkServiceV5 {
     public void sendMessage(String data) {
         String address = "http://example.com";
 
-        try (NetworkClientV5 client = new NetworkClientV5(address)) {
+        try (NetworkClientV5 client = new NetworkClientV5(address)) { //try-with-resources try()안에 객체 생성
+            //AutoCloseable을 구현한 객체만 사용 가능 - close()를 호출
             client.initError(data); //추가
             client.connect();
             client.send(data);
